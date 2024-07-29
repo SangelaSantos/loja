39bfd199371a42a5e75748f0f43f3c748f5a660c
SangelaSantos <sangelaufopa@gmail.com>
2024-07-29 09:17:40 -0300

    Projeto finalizado

--- src/components/feminino/style.js
+++ src/components/feminino/style.js
@@ -2,25 +2,26 @@ import styled from "styled-components";
 
 export const DivList = styled.div`
     width: 200px;
-    height: 180px;
-    background-color: white; 
+    height: 210px;
+    background-color: #F4F4F4; 
     padding: 10px;
     border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(39, 39, 39, 0.5), 0 -4px 8px rgba(39, 39, 39, 0.5), 4px 0 8px rgba(39, 39, 39, 0.5), -4px 0 8px rgba(39, 39, 39, 0.5);
-    border: 1px solid #272727;
+    box-shadow: 0 4px 8px rgba(39, 39, 39, 0.3), 0 -4px 8px rgba(39, 39, 39, 0.3), 4px 0 8px rgba(39, 39, 39, 0.3), -4px 0 8px rgba(39, 39, 39, 0.3);
 `;
 export const ButtonAdd = styled.button`
-    padding: 5px 30px;
+    padding: 5px 80px;
     margin-top: 8px;
     margin-left: 10px;
     border-radius: 5px;
-    background-color: #272727;
-    color: white;
+    background-color: white;
+    color: black;
     font-weight: bold;
-    transition: transform 0.2s ease;
+    border: 1px solid #272727;
     &:hover{
-        transform: scale(1.05);
+        background-color: black;
+        color: white;
     }
+
 `;
 export const ButtonQuantity = styled.button`
     padding: 5px 10px;
--- src/components/masculino/index.jsx
+++ src/components/masculino/index.jsx
@@ -1,38 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { database, ref, onValue, set, update } from '../../firebase';
-import { ButtonAdd, ButtonQuantity, DivList } from './style';
+import React, { useEffect, useState } from "react";
+import { database, ref, onValue, set, update } from "../../firebase";
+import { ButtonAdd, ButtonQuantity, DivList } from "./style";
 import { MdAddShoppingCart } from "react-icons/md";
 import { getAuth } from "firebase/auth";
 
 const MascList = () => {
   const [products, setProducts] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [quantities, setQuantities] = useState({});
   const auth = getAuth();
   const user = auth.currentUser;
 
   useEffect(() => {
     const fetchProducts = () => {
-      const categoryRef = ref(database, 'category');
-      onValue(categoryRef, (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          const productsList = [];
-          for (let category in data) {
-            for (let id in data[category]) {
-              if (category === 'masculino') {
-                productsList.push({ id, ...data[category][id], category });
+      const categoryRef = ref(database, "category");
+      onValue(
+        categoryRef,
+        (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            const productsList = [];
+            for (let category in data) {
+              for (let id in data[category]) {
+                if (category === "masculino") {
+                  productsList.push({ id, ...data[category][id], category });
+                }
               }
             }
+            setProducts(productsList);
+          } else {
+            setProducts([]);
           }
-          setProducts(productsList);
-        } else {
-          setProducts([]);
+        },
+        (error) => {
+          setError("Error fetching products.");
+          console.error("Error:", error);
         }
-      }, (error) => {
-        setError('Error fetching products.');
-        console.error('Error:', error);
-      });
+      );
     };
 
     fetchProducts();
@@ -40,22 +44,26 @@ const MascList = () => {
 
   const handleAddToCart = (product) => {
     if (!user) {
-      alert('Você precisa estar logado para adicionar itens ao carrinho.');
+      alert("Você precisa estar logado para adicionar itens ao carrinho.");
       return;
     }
 
     const quantity = quantities[product.id] || 1;
     const cartRef = ref(database, `cart/${user.uid}/${product.id}`);
 
-    onValue(cartRef, (snapshot) => {
-      const existingProduct = snapshot.val();
-      if (existingProduct) {
-        const newQuantity = existingProduct.quantity + quantity;
-        update(cartRef, { ...product, quantity: newQuantity });
-      } else {
-        set(cartRef, { ...product, quantity });
-      }
-    }, { onlyOnce: true });
+    onValue(
+      cartRef,
+      (snapshot) => {
+        const existingProduct = snapshot.val();
+        if (existingProduct) {
+          const newQuantity = existingProduct.quantity + quantity;
+          update(cartRef, { ...product, quantity: newQuantity });
+        } else {
+          set(cartRef, { ...product, quantity });
+        }
+      },
+      { onlyOnce: true }
+    );
 
     alert(`${quantity} ${product.name} foi adicionado ao carrinho!`);
   };
@@ -75,34 +83,70 @@ const MascList = () => {
   };
 
   return (
-    <div style={{ marginLeft: "170px"}}>
+    <div style={{ marginLeft: "170px" }}>
       <h3>Seção Masculina</h3>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       {products.length === 0 ? (
         <p>Não há produtos masculinos disponíveis.</p>
       ) : (
-        <ul style={{display: "flex", flexWrap: "wrap",listStyleType: 'none'}}>
+        <ul
+          style={{ display: "flex", flexWrap: "wrap", listStyleType: "none" }}
+        >
           {products.map((product) => (
-            <li key={product.id} style={{ marginBottom: '1em', margin: "0 20px 20px 20px" }}>
+            <li
+              key={product.id}
+              style={{ marginBottom: "1em", margin: "0 20px 20px 20px" }}
+            >
               <DivList>
-                <div style={{display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
                   {product.imageUrl && (
                     <img
                       src={product.imageUrl}
                       alt={product.name}
-                      style={{ width: '100px', height: '100px', objectFit: 'cover', marginRight: '1em'}}
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        marginRight: "1em",
+                        marginBottom: "10px"
+                      }}
                     />
                   )}
                 </div>
                 <div>
-                  <div style={{display: "flex", marginTop: "10px"}}>
-                  <div style={{fontWeight: "bold"}}>{product.name}</div>
-                  <div style={{marginLeft: "10px", color: "red", fontWeight: "bold"}}>R$ {product.price}</div>
+                  {product.name}
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "5px"}}>
+                    <ButtonQuantity
+                      onClick={() => handleDecreaseQuantity(product.id)}
+                    >
+                      -
+                    </ButtonQuantity>
+                    {quantities[product.id] || 1}
+                    <ButtonQuantity
+                      onClick={() => handleIncreaseQuantity(product.id)}
+                    >
+                      +
+                    </ButtonQuantity>
+                    <div
+                      style={{
+                        marginLeft: "5px",
+                        color: "red",
+                        fontWeight: "bold",
+                        fontSize: "22px"
+                      }}
+                    >
+                      R$ {product.price}
+                    </div>
                   </div>
-                      <ButtonQuantity onClick={() => handleDecreaseQuantity(product.id)}>-</ButtonQuantity>
-                      {quantities[product.id] || 1}
-                      <ButtonQuantity onClick={() => handleIncreaseQuantity(product.id)}>+</ButtonQuantity>
-                  <ButtonAdd onClick={() => handleAddToCart(product)}><MdAddShoppingCart style={{fontSize: "20px"}}/></ButtonAdd>
+                  <ButtonAdd onClick={() => handleAddToCart(product)}>
+                    <MdAddShoppingCart style={{ fontSize: "20px" }} />
+                  </ButtonAdd>
                 </div>
               </DivList>
             </li>
